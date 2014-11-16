ea25cfefb5297ca7b44ee2c23b0aa99a9ce4ddc2
Garath21 <thibaut_1000@hotmail.com>
2014-11-16 22:25:51 +0100

    Ajout du premier prototype

--- Code/Pi-Injector/src/launcher/Launcher.java
+++ Code/Pi-Injector/src/launcher/Launcher.java
@@ -1,9 +1,39 @@
 package launcher;
 
+import injectionTasks.LDAPInjectionTask;
+
+import org.jppf.client.JPPFClient;
+import org.jppf.client.JPPFJob;
+
+import scripts.LDAPScript;
+
+
 public class Launcher {
 
 	public static void main(String[] args) {
-		System.out.println("Projet Pi-Injector");
+		
+		System.out.println("Launching...");
+		
+		try (JPPFClient jppfClient = new JPPFClient()) {
+			ApplicationRunner runner = new ApplicationRunner();
+			
+			JPPFJob jppfJob = new JPPFJob();
+			jppfJob.setName("LDAP Injection Job");
+			
+			LDAPScript ldapScript = new LDAPScript("192.168.1.28", 10389);
+			ldapScript.addBindRequest("uid=admin,ou=system", "secret");
+			
+			
+			LDAPInjectionTask jppfTask = new LDAPInjectionTask(ldapScript);
+			jppfTask.setId(jppfJob.getName() + " - Task 1");
+			jppfJob.add(jppfTask, (Object[])null);
+			
+			
+			runner.executeBlockingJob(jppfClient, jppfJob);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
