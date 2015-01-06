e9204c2b63b909256f8e266bbff08a7e5dd53900
Garath21 <thibaut_1000@hotmail.com>
2015-01-06 21:28:55 +0100

    Ajout de nouvelles requêtes dans le LDAPScript et modification du GenericResult.sh

--- Code/Pi-Injector-Full/src/main/java/scripts/LDAPScript.java
+++ Code/Pi-Injector-Full/src/main/java/scripts/LDAPScript.java
@@ -4,7 +4,13 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.directory.api.ldap.model.entry.DefaultEntry;
+import org.apache.directory.api.ldap.model.entry.Entry;
 import org.apache.directory.api.ldap.model.exception.LdapException;
+import org.apache.directory.api.ldap.model.message.SearchRequest;
+import org.apache.directory.api.ldap.model.message.SearchRequestImpl;
+import org.apache.directory.api.ldap.model.message.SearchScope;
+import org.apache.directory.api.ldap.model.name.Dn;
 import org.apache.directory.ldap.client.api.LdapConnection;
 import org.apache.directory.ldap.client.api.LdapNetworkConnection;
 
@@ -70,6 +76,116 @@ public class LDAPScript extends AbstractScript {
 		} catch (LdapException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	
+	public void addUnbindRequest() {
+		
+		addNewMethod(
+				"executeUnbindRequest",
+				new Class[] {},
+				new ArrayList<Object>());
+	}
+	
+	public void executeUnbindRequest() {
+		
+		try {
+			connection.unBind();
+		} catch (LdapException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public void addAddRequest(String entryDN) {
+		
+		List<Object> params = new ArrayList<>();
+		params.add(entryDN);
+		
+		addNewMethod(
+				"executeAddRequest",
+				new Class[] {String.class},
+				params);
+	}
+	
+	public void executeAddRequest(String entryDN) {
+		
+		try {
+			Entry entry = new DefaultEntry(entryDN);
+			connection.add(entry);
+		} catch (LdapException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public void addDeleteRequest(String dn) {
+		
+		List<Object> params = new ArrayList<>();
+		params.add(dn);
+		
+		addNewMethod(
+				"executeDeleteRequest",
+				new Class[] {String.class},
+				params);
+	}
+	
+	public void executeDeleteRequest(String dn) {
+		
+		try {
+			connection.delete(dn);
+		} catch (LdapException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public void addSearchRequest(String dn, String filter, SearchScope scope) {
+		
+		List<Object> params = new ArrayList<>();
+		params.add(dn);
+		params.add(filter);
+		params.add(scope);
+		
+		addNewMethod(
+				"executeSearchRequest",
+				new Class[] {String.class, String.class, SearchScope.class},
+				params);
+	}
+	
+	public void executeSearchRequest(String dn, String filter, SearchScope scope) {
+		
+		SearchRequest req = new SearchRequestImpl();
+
+		try {
+			req.setBase(new Dn(dn));
+			req.setFilter(filter);
+			req.setScope(scope);
+			
+			connection.search(req);
+		} catch (LdapException e) {
+			e.printStackTrace();
+		}
+	}
+	
+
+	public void addModifyRequest() {
+		
+		//addNewMethod("executeModifyRequest");
+	}
+	
+	public void executeModifyRequest() {
+		
+	}
+	
+	
+	public void addRenameRequest() {
+		
+		//addNewMethod("executeRenameRequest");
+	}
+	
+	public void executeRenameRequest() {
+		
 	}
 	
 	
